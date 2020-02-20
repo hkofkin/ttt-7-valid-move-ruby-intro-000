50f69472059afb9f057350cd4dfe62fe21c6bba6
Hannah Kofkin <hkofkin@gmail.com>
2020-02-20 23:32:56 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -3,7 +3,7 @@ def valid_move?(board, index)
   if board[index] .between?(0, 8) && position_taken?(board, index) == false
     return true
   end
-  if board[index] !.between?(0,8) && position_taken?(board, index) == true
+  if board[index] .between?(0, 8) == false && position_taken?(board, index) == true
     return false
 end
 
