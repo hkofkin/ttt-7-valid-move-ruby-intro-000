78e86c76bb176b9ee2c2a6ab3ed515c49ea21c1c
Hannah Kofkin <hkofkin@gmail.com>
2020-02-20 23:47:55 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,7 +2,7 @@
 require 'pry'
 def valid_move?(board, index)
   binding.pry
-  if board[index].between?(0, 8) && position_taken?(board, index) == false
+  if board[index].between?(0, 8) == true && position_taken?(board, index) == false
     return true
   end
   if board[index].between?(0, 8) == false && position_taken?(board, index) == true
